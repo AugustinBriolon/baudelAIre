83f61615a5b04739dd80020a3a577d90a7a8ee83
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-27 18:30:58 +0100

    :lipstick: imprive ui

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,18 @@ export default function Home() {
     }
   }
 
+
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center py-32 bg-white">
+    <main className="flex min-h-screen w-full flex-col items-center justify-start py-32 bg-white">
       <div className="w-full max-w-xl flex flex-col items-center gap-8">
         <div className="flex flex-col items-center gap-2">
-          <h1 className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent  drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]">
+          <h1 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]">
             baudel<span className="text-blue-500">AI</span>re
           </h1>
-          <p className="text-center text-gray-500 [text-wrap:balance] md:text-xl">Génération de poème grâçe à baudelAIre</p>
+          <p className="text-center text-gray-500 [text-wrap:balance] md:text-xl">Génération de poème grâçe à l'<span className="font-bold">Intelligence Artificielle</span>.</p>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center gap-4 w-full">
           <form action={handleClick} className="w-full">
             <label className="sr-only">Your message</label>
             <div className="w-full flex items-center gap-4 rounded-lg">
@@ -38,16 +39,8 @@ export default function Home() {
             </div>
           </form>
 
+          <TextAnimation key={output} output={output} />
 
-          {output ?
-            <div className="relative mx-auto aspect-square w-full max-w-xl">
-              <div className='w-full h-fit min-h-96 p-4 resize-none rounded-2xl border border-gray-200 bg-white text-black'>
-                {output}
-              </div>
-            </div>
-            :
-            <TextAnimation />
-          }
         </div>
 
       </div>
--- components/TextAnimation.tsx
+++ components/TextAnimation.tsx
@@ -2,30 +2,56 @@ import { TypeAnimation } from 'react-type-animation';
 
 import poemsList from '../app/utils/poemsList';
 
-export function TextAnimation() {
+export function TextAnimation({ output }: { output: string }) {
   const poems = poemsList
   const poem1 = `${poems[0].title} \n\n ${poems[0].content} \n\n ${poems[0].author}`
   const poem2 = `${poems[1].title} \n\n ${poems[1].content} \n\n ${poems[1].author}`
   const poem3 = `${poems[2].title} \n\n ${poems[2].content} \n\n ${poems[2].author}`
 
+  const copyOutput = () => {
+    navigator.clipboard.writeText(output)
+  }
+
   return (
-    <div className="mx-auto mt-6 aspect-square w-full max-w-xl">
-      <div className='w-full h-fit min-h-96 p-4 resize-none rounded-2xl border border-gray-200 bg-white text-black'>
-        <TypeAnimation
-          sequence={[
-            poem1,
-            1000,
-            poem2,
-            1000,
-            poem3,
-            1000,
-          ]}
-          speed={99}
-          style={{ whiteSpace: 'pre-line' }}
-          wrapper="span"
-          cursor={false}
-          repeat={Infinity}
-        />
+    <div className="h-fit mx-auto w-full max-w-xl relative">
+      <div className='w-full min-h-96 max-h-fit p-4 resize-none rounded-2xl border border-gray-200 bg-white text-black'>
+        {
+          output.length === 0 ?
+            <TypeAnimation
+              sequence={[
+                poem1,
+                1000,
+                poem2,
+                1000,
+                poem3,
+                1000,
+              ]}
+              speed={99}
+              style={{ whiteSpace: 'pre-line' }}
+              wrapper="span"
+              cursor={false}
+              repeat={Infinity}
+            />
+            :
+            <div className='h-full w-full relative'>
+              <TypeAnimation
+                sequence={[
+                  output
+                ]}
+                speed={50}
+                style={{ whiteSpace: 'pre-line' }}
+                wrapper="span"
+                cursor={false}
+              />
+              <span className="text-blue-500 font-display text-2xl font-bold absolute right-2 bottom-2" >AI</span>
+              <div className="h-4 w-4 absolute right-0 top-0 text-slate-400 hover:text-slate-600 cursor-pointer" onClick={copyOutput}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+                  <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+                </svg>
+              </div>
+            </div>
+        }
       </div>
     </div>
   )
