23faf3f1c152b1e299345fe6707f951e7b377cbb
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-27 15:40:44 +0100

    :sparkles: add content home

--- components/Textarea.tsx
+++ components/Textarea.tsx
@@ -1,9 +1,60 @@
+'use client'
 
+import React, { useState, useEffect } from 'react';
+import poemsList from '../app/utils/poemsList';
+import { log } from 'console';
 
-export function Textarea() {
+export function Textarea(): JSX.Element {
+  const poems = poemsList;
+  const [currentPoemIndex, setCurrentPoemIndex] = useState<number>(0);
+  const [poemText, setPoemText] = useState<string>('');
+  const typingSpeed = 5; // Adjust typing speed as needed
+  const [rowsNumber, setRowsNumber] = useState<number>(0);
+
+  // setRowsNumber set on the nomber of lines of the poem
+  useEffect(() => {
+    const poem = poems[currentPoemIndex];
+    const lines = poem.content.split('\n').length;
+    console.log(lines);
+
+    setRowsNumber(lines + 5);
+  }, [currentPoemIndex, poems]);
+
+  useEffect(() => {
+    let currentIndex = 0;
+    let text = '';
+
+    const typeWriter = () => {
+      const poem = poems[currentPoemIndex];
+
+      text = poem.title + '\n\n' + poem.content + '\n\n' + poem.author;
+
+      setPoemText(text.slice(0, currentIndex));
+
+      currentIndex++;
+
+      if (currentIndex <= text.length) {
+        setTimeout(typeWriter, typingSpeed);
+      } else {
+        setTimeout(() => {
+          setCurrentPoemIndex((prevIndex) => (prevIndex + 1) % poems.length);
+          setPoemText('');
+        }, 2000); // Wait 2 seconds before displaying the next poem
+      }
+    };
+
+    typeWriter();
+  }, [currentPoemIndex, poems]);
+
   return (
-    <div className="relative mx-auto mt-6 aspect-square w-full max-w-xl overflow-hidden rounded-2xl border border-gray-200 " >
-      <textarea className="w-full h-full p-4 bg-gray-50 resize-none" placeholder="Saisissez votre texte ici..."></textarea>
+    <div className="relative mx-auto mt-6 aspect-square w-full max-w-xl">
+      <textarea
+        rows={rowsNumber}
+        className="w-full h-fit min-h-96 p-4 resize-none rounded-2xl border border-gray-200 bg-white"
+        disabled
+        placeholder="Saisissez votre texte ici..."
+        value={poemText}
+      ></textarea>
     </div>
   );
 }
