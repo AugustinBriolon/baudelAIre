d47a00160557b80d5d5fd9978656c877230ea736
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-27 19:09:22 +0100

    :bug: fix deploy bug

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           <h1 className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-5xl font-bold text-transparent md:text-7xl">
             baudel<span className="text-blue-500">AI</span>re
           </h1>
-          <p className="text-center text-gray-500 [text-wrap:balance] md:text-xl">Génération de poème grâçe à l'<span className="font-bold">Intelligence Artificielle</span>.</p>
+          <p className="text-center text-gray-500 [text-wrap:balance] md:text-xl">Génération de poème grâçe à l&apos;<span className="font-bold">Intelligence Artificielle</span>.</p>
         </div>
 
         <div className="flex flex-col items-center gap-4 w-full">
