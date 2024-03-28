cfc99627e1cf16a06ed77170d4704ce0e3690a60
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-28 12:19:57 +0100

    :lipstick: imprive ui

--- components/Select.tsx
+++ components/Select.tsx
@@ -12,7 +12,7 @@ export default function Select({
   return (
     <div className="flex flex-col items-start w-full">
       <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-      <select className="bg-white border border-gray-200 text-gray-900 text-sm rounded-lg block w-full p-2.5" onChange={(e) => setData({ [name]: e.target.value })}>
+      <select className="appearance-none md:appearance-auto bg-white border border-gray-200 text-gray-900 text-sm rounded-lg block w-full p-2.5" onChange={(e) => setData({ [name]: e.target.value })}>
         <option value="">Sélectionnez une option</option>
         {data.map((item, index) => (
           <option key={index} value={item}>{item}</option>
