9ecdcacedcd875303f8f02285153bd55a9cbe85b
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-28 10:35:30 +0100

    :mag: ogimage

--- components/TextAnimation.tsx
+++ components/TextAnimation.tsx
@@ -23,7 +23,7 @@ export function TextAnimation({ output, isLoading, stop }: { output: string; isL
           <div className="h-full w-textContainer">
             <output style={{ whiteSpace: "pre-wrap" }}>{output}</output>
             <div className="absolute top-0 right-0 h-full p-2 flex flex-col items-center justify-between">
-              <div className="flex flex-col items-center gap-2P">
+              <div className="flex flex-col items-center gap-2">
                 <div className="h-8 w-8 p-2 text-gray-400 hover:text-gray-600 cursor-pointer border border-gray-200 rounded-lg" onClick={copyOutput}>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
