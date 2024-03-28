3662fd299c4cfc91e3ab714b0ed8f158b5f7bdb4
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-28 14:41:04 +0100

    :bug: fix select defautl value

--- components/Select.tsx
+++ components/Select.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-
 export default function Select({ label, data, setData, name, disabled }: { label: string; data: string[] | { name: string; characteristics: string }[]; setData: (data: any) => void; name: string; disabled?: boolean }) {
 
   const renderOption = (item: string | { name: string; characteristics: string }, index: number) => {
@@ -33,7 +31,7 @@ export default function Select({ label, data, setData, name, disabled }: { label
     <div className="flex flex-col items-start w-full">
       <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
       <select className={`${disabled ? "cursor-not-allowed bg-gray-100" : "cursor-pointer bg-white"} appearance-none md:appearance-auto border border-gray-200 text-gray-900 text-sm rounded-lg block w-full p-2.5`} onChange={handleChange} disabled={disabled}>
-        <option value="">Sélectionnez une option</option>
+        <option value={name === "temperatureType" ? "1" : ""}>Sélectionnez une option</option>
         {data.map(renderOption)}
       </select>
     </div>
