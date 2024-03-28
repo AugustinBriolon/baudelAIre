8c2876b2eeba25c08ff6e9af2f5e1e02689c99e4
Augustin Briolon <augustin.briolon@fortuneo.com>
2024-03-28 10:32:38 +0100

    :sparkles: Mise à jour du composant Select

--- components/Select.tsx
+++ components/Select.tsx
@@ -3,18 +3,17 @@ export default function Select({
   data,
   setData,
   name
-}:
-  {
-    label: string,
-    data: string[],
-    setData: (data: any) => void,
-    name: string,
-  }) {
+}: {
+  label: string,
+  data: string[],
+  setData: (data: any) => void,
+  name: string,
+}) {
   return (
     <div className="flex flex-col items-start w-full">
       <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-      <select defaultValue="default" id="countries" className="bg-white border border-gray-200 text-gray-900 text-sm rounded-lg block w-full p-2.5" onChange={(e) => setData({ [name]: e.target.value })}>
-        <option value="default" disabled>Sélectionnez une option</option>
+      <select className="bg-white border border-gray-200 text-gray-900 text-sm rounded-lg block w-full p-2.5" onChange={(e) => setData({ [name]: e.target.value })}>
+        <option value="">Sélectionnez une option</option>
         {data.map((item, index) => (
           <option key={index} value={item}>{item}</option>
         ))}
